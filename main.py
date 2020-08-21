ca0eacf4135243df8c90bfc1a5d1f4fd08ede4b0
Polina <tatsuchan@mail.ru>
2020-08-21 17:02:39 +0300

    'first'

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import List
+from fastapi import FastApi
+from tortoise.contrib.fastapi import register_tortoise
+from .models import Item, ItemPydantic, ItemInPydantic
+
+app = FastApi()
+
+
+
+@app.post('/items', response_model=ItemPydantic)
+async def create_rate(item: ItemInPydantic):
+	item_obj = await Item.create(**item.dict(exclude_unset=True))
+	return await ItemPydantic.from_tortoise_orm(item_obj)
+
+
+@app.get('/items', response_model=List[ItemPydantic])
+async def get_rate():
+	return await ItemPydantic.from_queryset(Item.all())
+
+
+register_tortoise(
+    app,
+    db_url='sqlite://sql_app.db',
+	models={'models':['models']},
+	generate_schemas=True,
+	add_exception_handlers=True,
+)
---
+++ models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from tortoise import models, fields
+from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
+
+
+
+class Item(models.Model):
+	"""A model of items
+	"""
+	id = fields.ForeignKeyField(models.Date, related_name='items')
+	cargo_type = fields.TextField()
+	rate = fields.FloatField()
+	date = fields.DateField(auto_now=True)
+
+	async def save(self, *args, **kwargs) -> None:
+		self.rate = 12.5
+		await super().save(*args, **kwargs)
+
+	class PydanticMeta:
+		allow_cycles = False
+
+
+#Pydantic schemas
+ItemPydantic = pydantic_model_creator(Item, name='Items')
+ItemInPydantic = pydantic_model_creator(Item, name='ItemsIn', exclude_readonly=True)
